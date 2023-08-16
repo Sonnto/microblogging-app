64d1f788cda7950e1cf96822b5563908a8e6f440
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-08-16 19:06:21 -0400

    Change logo to p tag

--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -7,7 +7,7 @@ const SidebarLogo = () => {
       onClick={() => router.push("/")}
       className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-gray-300 hover:bg-opacity-10 cursor-pointer transition"
     >
-      <div className="text-3xl text-gray-300">𝕏</div>
+      <p className="text-3xl text-gray-300">𝕏</p>
     </div>
   );
 };
