9e2feb16b0a648621a843ffd6663c069b3265022
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-08-16 19:38:49 -0400

    shrink post button size

--- components/layout/SidebarPostButton.tsx
+++ components/layout/SidebarPostButton.tsx
@@ -9,8 +9,8 @@ const SidebarPostButton = () => {
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-venter justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
         <BsPencilSquare size={24} color="white" />
       </div>
-      <div className="mt-6 hidden lg:block px-4 py-4 rounded-full bg-sky-500 hover:bg-opacity-90 transition">
-        <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
+      <div className="mt-6 hidden lg:block px-3 py-3 rounded-full bg-sky-500 hover:bg-opacity-90 transition">
+        <p className="hidden lg:block text-center font-semibold text-gray-200 text-[20px]">
           Create Post
         </p>
       </div>
