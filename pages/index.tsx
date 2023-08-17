66dc8e66b61d6084e24fc92231a346abd97cc522
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-08-17 15:18:58 -0400

    Stylize index, adding Home Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <div className="text-3xl text-gray-200">Hello World</div>;
+  return (
+    <>
+      <Header label="Home" />
+    </>
+  );
 }
