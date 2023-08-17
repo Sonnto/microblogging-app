09b5faee5ff5129147dd433e7a44a15aac28822e
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-08-17 14:42:16 -0400

    Update SidebarPostButton.tsx

--- components/layout/SidebarPostButton.tsx
+++ components/layout/SidebarPostButton.tsx
@@ -3,7 +3,6 @@ import { BsPencilSquare } from "react-icons/bs";
 
 const SidebarPostButton = () => {
   const router = useRouter();
-
   return (
     <div onClick={() => router.push("/")}>
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-venter justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
