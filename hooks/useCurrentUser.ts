7374829ddfee765856ce1d96f4832c9dc3bedc1c
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-09-01 12:48:02 -0400

    Update useCurrentUser.ts

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -4,8 +4,9 @@ import fetcher from "@/libs/fetcher";
 
 const useCurrentUser = () => {
   // Fetch current user data using the useSWR hook.
+  // Global state management SWR is a global state management library that allows you to fetch data, cache it, and revalidate it.
   const { data, error, isLoading, mutate } = useSWR("/api/current", fetcher);
-
+  // Will check if data requires revalidated; if so, it will revalidate the data and update the cache; if not, it will return the cached data.
   return {
     data, // User data fetched from the API endpoint.
     error, // Any error that occurred during the fetch.
