13b1a2411e56cba023b7090fb0b7eb8dd994bde5
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-09-06 17:37:52 -0400

    Update FollowBar.tsx

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -1,9 +1,21 @@
+import useUsers from "@/hooks/useUsers";
+
 const FollowBar = () => {
+  // const { data: users = [] } = useUsers();
+
+  // if (users.length === 0) {
+  //   return null;
+  // }
+
   return (
     <div className="px-6 py-4 hidden lg:block">
       <div className="bg-neutral-800 rounded-xl p-4">
         <h2 className="text-gray-200 text-xl font-semibold">Who to Follow</h2>
-        <div className="flex flex-col gap-6 mt-4">{/*TODO: USER LIST*/}</div>
+        <div className="flex flex-col gap-6 mt-4">
+          {/* {users.map((user: Record<string, any>) => (
+            <div key={user.id} className="flex flex-row gap-4"></div>
+          ))} */}
+        </div>
       </div>
     </div>
   );
